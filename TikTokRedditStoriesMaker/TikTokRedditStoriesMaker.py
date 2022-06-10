274047fbea7faf0a24cd9f9ba4c7c7dc8de3e3e6
BakyJelou <thedanman740@gmail.com>
2022-06-10 21:00:51 +0200

    added options to use inbuilt TTS or use custom MP3

--- TikTokRedditStoriesMaker/TikTokRedditStoriesMaker.py
+++ TikTokRedditStoriesMaker/TikTokRedditStoriesMaker.py
@@ -10,6 +10,11 @@ import os.path
 init()
 print("\nTikTok Reddit video maker by")
 print(Fore.YELLOW + pyfiglet.figlet_format("BakyJelou"))
+tts_engine = "none";
+if input("Do you want to use in-built TTS engine? (Y/N): ") == "Y":
+    tts_engine = "inbuilt"
+else:
+    tts_engine = "custom"
 print(Style.RESET_ALL + "\nTrying to find visuals...")
 if os.path.isfile("assets/background.mp4"):
     background = VideoFileClip("assets/background.mp4")
@@ -33,36 +38,53 @@ else:
     exit()
 
 print(Style.RESET_ALL + "\nTrying to find Text-To-Speech...")
-if os.path.isfile("assets/txt_title.txt"):
-    txt_title = open("assets/txt_title.txt",'r',encoding="utf-8").read()
-    print("txt_title.txt [" + Fore.GREEN + "✔️" + Style.RESET_ALL +"]")
+if tts_engine == "inbuilt":
+    if os.path.isfile("assets/txt_title.txt"):
+        txt_title = open("assets/txt_title.txt",'r',encoding="utf-8").read()
+        print("txt_title.txt [" + Fore.GREEN + "✔️" + Style.RESET_ALL +"]")
+    else:
+        print("txt_title.txt [" + Fore.RED + "❌" + Style.RESET_ALL +"]" + " not found, please add txt_title.txt to the folder")
+        exit()
+
+    if os.path.isfile("assets/txt_text.txt"):
+        txt_text = open("assets/txt_text.txt",'r',encoding="utf-8").read()
+        print("txt_text.txt [" + Fore.GREEN + "✔️" + Style.RESET_ALL +"]")
+    else:
+        print("txt_text.txt [" + Fore.RED + "❌" + Style.RESET_ALL +"]" + " not found, please add txt_text.txt to the folder")
+        exit()
 else:
-    print("txt_title.txt [" + Fore.RED + "❌" + Style.RESET_ALL +"]" + " not found, please add txt_title.txt to the folder")
-    exit()
+    if os.path.isfile("assets/tts_title.mp3"):
+        tts_title = AudioFileClip('assets/tts_title.mp3')
+        print("tts_title.mp3 [" + Fore.GREEN + "✔️" + Style.RESET_ALL +"]")
+    else:
+        print("tts_title.mp3 [" + Fore.RED + "❌" + Style.RESET_ALL +"]" + " not found, please add tts_title.mp3 to the folder")
+        exit()
 
-if os.path.isfile("assets/txt_text.txt"):
-    txt_text = open("assets/txt_text.txt",'r',encoding="utf-8").read()
-    print("txt_text.txt [" + Fore.GREEN + "✔️" + Style.RESET_ALL +"]")
-else:
-    print("txt_text.txt [" + Fore.RED + "❌" + Style.RESET_ALL +"]" + " not found, please add txt_text.txt to the folder")
-    exit()
+    if os.path.isfile("assets/tts_text.mp3"):
+        tts_text = AudioFileClip('assets/tts_text.mp3')
+        print("tts_text.mp3 [" + Fore.GREEN + "✔️" + Style.RESET_ALL +"]")
+    else:
+        print("tts_text.mp3 [" + Fore.RED + "❌" + Style.RESET_ALL +"]" + " not found, please add tts_text.mp3 to the folder")
+        exit()
 
-print("\nProcessing Text-To-Speech...")
-engine = pyttsx3.init()
-rate = engine.getProperty('rate') 
-engine.setProperty('rate', rate-70)
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
-engine.save_to_file(txt_title,'assets/temp/tts_title.wav')
-engine.runAndWait()
-engine.save_to_file(txt_text,'assets/temp/tts_text.wav')
-engine.runAndWait()
 
-ffmpeg_extract_audio('assets/temp/tts_title.wav','assets/temp/tts_title.mp3')
-ffmpeg_extract_audio('assets/temp/tts_text.wav','assets/temp/tts_text.mp3')
+if tts_engine == "inbuilt":
+    print("\nProcessing Text-To-Speech...")
+    engine = pyttsx3.init()
+    rate = engine.getProperty('rate') 
+    engine.setProperty('rate', rate-70)
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+    engine.save_to_file(txt_title,'assets/temp/tts_title.wav')
+    engine.runAndWait()
+    engine.save_to_file(txt_text,'assets/temp/tts_text.wav')
+    engine.runAndWait()
 
-tts_title = AudioFileClip('assets/temp/tts_title.mp3')
-tts_text = AudioFileClip('assets/temp/tts_text.mp3')
+    ffmpeg_extract_audio('assets/temp/tts_title.wav','assets/tts_title.mp3')
+    ffmpeg_extract_audio('assets/temp/tts_text.wav','assets/tts_text.mp3')
+
+    tts_title = AudioFileClip('assets/tts_title.mp3')
+    tts_text = AudioFileClip('assets/tts_text.mp3')
 
 print("Processing data...")
 totallength = tts_title.duration + 1 + tts_text.duration
@@ -76,8 +98,10 @@ print("Processing audio...")
 audio_output = CompositeAudioClip([tts_title.set_start(0),tts_text.set_start(tts_title.duration + 1)])
 
 print("Processing video...,\n")
+title = title.resize(0.95)
 title = title.set_start(0)
 title = title.set_end(tts_title.duration)
+text = text.resize(0.80)
 text = text.set_start(tts_title.duration)
 video_output = CompositeVideoClip([backgroundclip,title.set_position("center"),text.set_position("center")])
 video_output.audio = audio_output
